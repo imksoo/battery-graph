f03ea9f4eab4f565469bbb9024664cb942dabb6e
Kirino Minato <kirino.minato@gmail.com>
2023-11-29 22:25:01 +0000

    Refactor getCurrentInfomationFromAiSEG2 function to return the entire JSON response

--- index.ts
+++ index.ts
@@ -28,25 +28,29 @@ async function getTotalValueFromAiSEG2(url: string, username: string, password: 
   return null;
 }
 
-async function getBatteryPercentageFromAiSEG2(url: string, username: string, password: string): Promise<number | null> {
+async function getCurrentInfomationFromAiSEG2(url: string, username: string, password: string): Promise<any> {
   const digestAuth = new AxiosDigestAuth({ username, password });
 
   try {
     const response = await digestAuth.request({ method: 'POST', url: url });
 
     const json_data = JSON.parse(JSON.stringify(response.data));
-    if (json_data && json_data.percent) {
-      return json_data.percent;
+    if (json_data) {
+      return json_data;
     }
   } catch (error) {
     console.error(error);
   }
-  return null;
+  return {};
 }
 
 /// メイン処理
 /// @param writeInfluxDB InfluxDBに書き込むかどうか
 async function main(writeInfluxDB: boolean = false) {
+  // 瞬間値の情報を取得
+  const currentInfomation1 = await getCurrentInfomationFromAiSEG2(`${baseUrl}/data/electricflow/111/update`, username, password) || {};
+  const currentInfomation2 = await getCurrentInfomationFromAiSEG2(`${baseUrl}/data/electricflow/112/update`, username, password) || {};
+
   // 発電量の値を取得
   const powerGenerated = await getTotalValueFromAiSEG2(`${baseUrl}/page/graph/51111`, username, password) || 0;
   // 買電量の値を取得
@@ -56,12 +60,22 @@ async function main(writeInfluxDB: boolean = false) {
   // 使用電力量の値を取得
   const usedPower = await getTotalValueFromAiSEG2(`${baseUrl}/page/graph/52111`, username, password) || 0;
   // 蓄電量の残量を取得
-  const batteryPercentage = await getBatteryPercentageFromAiSEG2(`${baseUrl}/data/electricflow/111/update`, username, password) || 0;
+  const batteryPercentage = Number.parseFloat(currentInfomation1.percent || "0.0");
 
   // 差し引き推定蓄電量を計算
   const estimatedCapacity = (powerGenerated + powerFromGrid) - (soldPowerToGrid + usedPower);
   // 蓄電池の放電可能な電力量を計算
   const remainingCapacity = ((batteryPercentage - batteryLowLimit) * batteryCapacity) / 100;
+
+  // 太陽光発電の発電量(瞬間値)を取得
+  const currentPVOutput = Number.parseFloat(currentInfomation2.lo_sun_lo_kW || "0.0");
+  // 家庭内の使用電力量(瞬間値)を取得
+  const currentUsedPower = Number.parseFloat(currentInfomation2.u_capacity || "0.0");
+  // 系統からの受電量(瞬間値)を取得
+  const currentGridPower = Number.parseFloat(currentInfomation2.lo_densen_lo_kw || "0.0");
+  // 蓄電池からの放電量(瞬間値)を取得
+  let _curentBatteryPower = Number.parseFloat(currentInfomation2.lo_battery_lo_kW || "0.0");
+  const curentBatteryPower = (currentInfomation2.charge === "0") ? _curentBatteryPower * -1.0 : _curentBatteryPower;
 
   // 結果をコンソールに出力
   console.log("--------------------------------------------------");
@@ -73,6 +87,10 @@ async function main(writeInfluxDB: boolean = false) {
   console.log('差し引き推定蓄電量: ', estimatedCapacity.toFixed(1), 'kWh');
   console.log('蓄電池 残パーセント: ', batteryPercentage.toFixed(0), '%');
   console.log('蓄電池 放電可能な電力量: ', remainingCapacity.toFixed(1), 'kWh');
+  console.log('太陽光発電の発電量(瞬間値): ', currentPVOutput.toFixed(1), 'kW');
+  console.log('家庭内の使用電力量(瞬間値): ', currentUsedPower.toFixed(1), 'kW');
+  console.log('系統からの受電量(瞬間値): ', currentGridPower.toFixed(1), 'kW');
+  console.log('蓄電池の入出力量(瞬間値): ', curentBatteryPower.toFixed(1), 'kW');
 
   // InfluxDBが設定されている場合は、InfluxDBに書き込み
   if (writeInfluxDB) {
@@ -91,6 +109,10 @@ async function main(writeInfluxDB: boolean = false) {
         .floatField('estimatedCapacity', estimatedCapacity)
         .floatField('batteryPercentage', batteryPercentage)
         .floatField('remainingCapacity', remainingCapacity)
+        .floatField('currentPVOutput', currentPVOutput)
+        .floatField('currentUsedPower', currentUsedPower)
+        .floatField('currentGridPower', currentGridPower)
+        .floatField('curentBatteryPower', curentBatteryPower)
     )
     influxWriter.flush();
     influxWriter.close();
